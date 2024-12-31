c686d402cf8f0ea9e874bf7ef4c1f5218c0c24d8
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-12-31 15:47:07 +0000

    2.0

--- serv/main_AI.py
+++ serv/main_AI.py
@@ -1,7 +1,22 @@
 from flask import Flask, request
 import tensorflow as tf
+import json
 
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras import layers
 
-app = Flask()
+app = Flask(__name__)
+
+#model loading
+model = load_model("model_LSTM.keras")
+
+#tokenizer loading
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = Tokenizer.from_json(data)
+    del data
+
+@app.route("/check-text")
+def check_text():
+    text = request.args["text"]
